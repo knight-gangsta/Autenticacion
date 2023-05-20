d082484526c47353edf2e8a5fb8dfa74b36e83d1
guasonsad <nikole2019@hotmail.com>
2023-05-20 00:02:31 -0500

    Login

--- Program.cs
+++ Program.cs
@@ -9,6 +9,12 @@ namespace Autenticacion
 			// Add services to the container.
 			builder.Services.AddRazorPages();
 
+			builder.Services.AddAuthentication().AddCookie("MyCookieAuth", options =>
+			{
+				options.Cookie.Name = "MyCookieAuth";
+				options.LoginPath = "/Account/Login";
+			});
+
 			var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
